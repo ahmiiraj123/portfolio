b21b5957e14fb0aa77cffe7de31602e16f605342
Ahmar Manzoor <156393599+ahmiiraj123@users.noreply.github.com>
2025-06-09 10:27:11 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+ // Change this single string whenever your Apps Script URL changes.
+ window.GAS_BASE_URL =
+ "https://script.google.com/macros/s/AKfycbwcDEp0447GqSWewr48UiXtKUXkAZDt7Zc_eQestRoAudjn1krTp94aKW59I2GpEONtSQ/exec";
